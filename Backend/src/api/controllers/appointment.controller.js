e3cc644e74abc5f7512f806b060b162a7e77e37d
MrsGlou <114435154+MrsGlou@users.noreply.github.com>
2023-07-24 13:10:13 +0200

    nails proyect

--- Backend/src/api/controllers/appointment.controller.js
+++ Backend/src/api/controllers/appointment.controller.js
@@ -7,13 +7,12 @@ dotenv.config();
 const create = async (req, res, next) => {
   try {
     await Appointment.syncIndexes();
-    //1. Recogemos todos los servicios y calculamos el tiempo que debe durar la cita sumandolos
-    //2. Revisamos las citas programadas
+    //1. Recogemos todos los servicios y calculamos el tiempo que debe durar la cita sumandolos 
+    //2. Revisamos las citas programadas(GET ALL -> appointment x calendario) En este punto hacenos tmb el 4 al recibirlas.
     //3. Obtenemos hora y fecha actual
-    //4. Ordena las citas programadas por fecha
-    //5. Calcula las citas disponibles en función de la duración de la cita
-    //6. Recibimos la cita que el cliente quiere reservar y guardamos
-
+    //4. Ordena las citas programadas por fecha()
+    //5. Calcula las citas disponibles en función de la duración de la cita(GET DISPONIBILITY -> Appointment)
+    //6. Calculamos la cita que el cliente quiere reservar y guardamos
     //7. Guardamos la cita
   } catch (error) {
     return next(
@@ -30,6 +29,19 @@ const create = async (req, res, next) => {
 //--------- DELETE APPOINTMENT ---------//
 //--------- GET BY EMAIL APPOINTMENT ---------//
 //--------- GET BY ID ---------//
+const getByID = async (req, res, next) => {
+  try {
+    const {id} = req. params;
+    const appointmentID = await Appointment.findById(id).populate("calendar")
+  } catch (error) {
+    return next(
+      setError(
+        500 || error.code,
+        error.message || 'General error get by ID appointment'
+      )
+    );
+  }
+}
 
 module.exports = {
   create,
